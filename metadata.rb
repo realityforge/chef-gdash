386df290b2bec32c693c64b6bd027d57136f2b67
Ajay Mamtura <ajay@ecmediallc.com>
2013-09-18 16:20:15 -0700

    adding apache2 digest authentication option support + 3 misc fixes
    misc fixes related to:
    - make log dir permissions as per common setup for /var/log/<dir> i.e. 755
    - error encountered during chef runs: "invalid regular expression; there's
      no previous pattern, to which '{' would define cardinality at 1: /{|}/"
    - provide attribute to be able to avoid duplicate NameVirtualHost directive
      so that one does not see "NameVirtualHost *:80 has no VirtualHosts"
      during apache2 startup.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'Peter@realityforge.org'
 license          'Apache 2.0'
 description      'Installs/Configures gdash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.3'
+version          '0.2.4'
 
 depends 'build-essential'
 depends 'graphite'
